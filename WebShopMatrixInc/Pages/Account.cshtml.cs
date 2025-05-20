8f450f27e65515ff0825448447c92c6a3f565408
ricklib <2403211vincken@zuyd.nl>
2025-05-20 20:35:23 +0200

    Progress Account page

--- WebShopMatrixInc/Pages/Account.cshtml.cs
+++ WebShopMatrixInc/Pages/Account.cshtml.cs
@@ -1,8 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLayer.Models;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
 
 namespace WebShopMatrixInc.Pages;
 
 public class AccountModel : PageModel
 {
+    private readonly ILogger<ProductViewModel> _logger;
+    private readonly CustomerRepository _customerRepository;
+    public Customer? Customer { get; set; } = new Customer();
+
+    public AccountModel(ILogger<ProductViewModel> logger, CustomerRepository customerRepository)
+    {
+        _logger = logger;
+        _customerRepository = customerRepository;
+    }
     
+    public void OnGet()
+    {
+        try
+        {
+            Customer = _customerRepository.GetCustomerById(5);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+        }
+    }
 }
